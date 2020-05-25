95441fd0e3f9f70f21132b858e99faed22c36c77
catmadf <375174102@qq.com>
2020-05-25 20:23:28 +0800

    。

--- src/main/java/cn/madf/basicKnowledge/WriteBin.java
+++ src/main/java/cn/madf/basicKnowledge/WriteBin.java
@@ -10,19 +10,22 @@ import java.nio.file.Paths;
  */
 public class WriteBin {
     public static void main(String[] args) throws IOException {
-        final int ixStart = 20;
-        byte[] buffer = new byte[23 * 4];
-        byte[] data = new byte[83 * 4];
+        final int ixStart = 7 - 1;
+        byte[] buffer = new byte[24 * 4];
+        byte[] data = new byte[80 * 4];
 
-        String filepath = "E:\\周界入侵监测\\所有数据\\分类数据\\0511刮风数据";
+        String filepath = "E:\\周界入侵监测\\所有数据\\0525\\新建文件夹";
 
         File root = new File(filepath);
         File[] files = root.listFiles();
         if (files != null) {
             for (File file : files) {
+                if (file.isDirectory()) {
+                    continue;
+                }
                 String[] s = file.getName().split("-");
 
-                String filename = "E:\\周界入侵监测\\所有数据\\分类数据\\0511刮风数据" + "\\" + "1-83-" + s[s.length - 1];
+                String filename = "E:\\周界入侵监测\\所有数据\\0525\\新建文件夹" + "\\" + "1-80-" + s[s.length - 1];
 
                 DataOutputStream outputStream = new DataOutputStream(new FileOutputStream(filename, true));
                 BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file));

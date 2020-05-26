66a28945b2a54e2204612178aa03b00fb6e03b08
catmadf <375174102@qq.com>
2020-05-26 10:05:50 +0800

    重做leetcode-4

--- src/main/java/cn/madf/leetCode/P4_FindMedianInTwoSortedArrays.java
+++ src/main/java/cn/madf/leetCode/P4_FindMedianInTwoSortedArrays.java
@@ -12,11 +12,11 @@ package cn.madf.leetCode;
  * 由于有两个数组，我们没法确定这个数在哪个数组，因此可以在每个数组上找各自第k/2 - 1小的数，每个数组被分位两部分
  * A[0...k/2-1]和A[k/2...n]、B[0...k/2-1]和B[k/2...m]
  * 将每个数组分成两部分，进行分类讨论，每次可以排除掉A或B中前半部分数组(k/2-1+k/2-1 = k-2, 所以第k小的元素不在这部分即将被排除的数组中)。
- *
- * tips：leetCode官方解法中定义了一个查找第k小的数的函数，通过调用该方法找到中位数本数(奇数)或者第k和k+1小的数(偶数)
- *       但是实际写的过程中发现偶数情况在找第k小的数时可以顺带将第k+1小的数一起找出来额外多出来的操作只需要常数项的复杂度就可解决
- *       算是一种优化，不过代码。
- *
+ * <p>
+ * 注，虽然偶数情况可以一次找出k和k+1的数，但没有意义，这样会使得代码结构很乱。
+ * <p>
+ * 毫无意义的优化不必进行，刷题不仅仅需要注意时间复杂度、空间复杂度，还需要时刻注意程序结构，保证逻辑清晰、接口完善
+ * <p>
  * 本题最优解 ：
  *
  * @author 烛影鸾书
@@ -27,81 +27,32 @@ public class P4_FindMedianInTwoSortedArrays {
 
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         if (nums1 == null || nums1.length == 0) {
-            return nums2.length % 2 == 0 ? (nums2[nums2.length / 2-1] + nums2[nums2.length / 2]) / 2.0 : nums2[nums2.length / 2];
+            return nums2.length % 2 == 0 ? (nums2[nums2.length / 2 - 1] + nums2[nums2.length / 2]) / 2.0 : nums2[nums2.length / 2];
         }
         if (nums2 == null || nums2.length == 0) {
-            return nums1.length % 2 == 0 ? (nums1[nums1.length / 2-1] + nums1[nums1.length / 2]) / 2.0 : nums1[nums1.length / 2];
+            return nums1.length % 2 == 0 ? (nums1[nums1.length / 2 - 1] + nums1[nums1.length / 2]) / 2.0 : nums1[nums1.length / 2];
         }
         int len1 = nums1.length, len2 = nums2.length;
+
         int k;
-        if ((len1 + len2) % 2 == 0) {
-            k = (len1 + len2) / 2 - 1 + 1;
-        } else {
+
+        /* 对于奇数情况 */
+        if ((len1 + len2) % 2 == 1) {
             k = (len1 + len2) / 2 + 1;
+            return getKthSmallestNum(nums1, nums2, k);
+        } else {
+            k = (len1 + len2) / 2;
+            return (getKthSmallestNum(nums1, nums2, k) + getKthSmallestNum(nums1, nums2, k + 1)) / 2.0;
         }
-        int offset1 = 0, offset2 = 0;
 
-        while (true) {
-
-            if (offset1 == len1) {
-                if ((len1 + len2) % 2 == 0) {
-                    return (nums2[offset2 + k - 1] + nums2[offset2 + k]) / 2.0;
-                } else {
-                    return nums2[offset2 + k - 1];
-                }
-            }
-            if (offset2 == len2) {
-                if ((len1 + len2) % 2 == 0) {
-                    return (nums1[offset1 + k - 1] + nums1[offset1 + k]) / 2.0;
-                } else {
-                    return nums1[offset1 + k - 1];
-                }
-            }
-
-            if (k == 1) {
-
-                if (nums1[offset1] < nums2[offset2]) {
-                    if ((len1 + len2) % 2 != 0) {
-                        return nums1[offset1];
-                    }
-                    if (offset1 + 1 == len1) {
-                        return (nums1[offset1] + nums2[offset2]) / 2.0;
-                    } else {
-                        return (nums1[offset1] + Math.min(nums2[offset2], nums1[offset1 + 1])) / 2.0;
-                    }
-                } else if (nums1[offset1] > nums2[offset2]) {
-                    if ((len1 + len2) % 2 != 0) {
-                        return nums2[offset2];
-                    }
-                    if (offset2 + 1 == len2) {
-                        return (nums2[offset2] + nums1[offset1]) / 2.0;
-                    } else {
-                        return (nums2[offset2] + Math.min(nums1[offset1], nums2[offset2 + 1])) / 2.0;
-                    }
-                } else {
-                    return nums1[offset1];
-                }
-            }
-
-            /* 将两个数组[offset, index]的数比较，抛弃其中一组小的 */
-            int index1 = Math.min(offset1 + (k >> 1) - 1, len1 - 1);
-            int index2 = Math.min(offset2 + (k >> 1) - 1, len2 - 1);
-
-            if (nums1[index1] <= nums2[index2]) {
-                k = k - (index1 - offset1 + 1);
-                offset1 = index1 + 1;
-            } else {
-                k = k - (index2 - offset2 + 1);
-                offset2 = index2 + 1;
-            }
-        }
     }
 
     /**
      * 在两个数中找到第k小的数
+     *
      * @param nums1 array
      * @param nums2 array
-     * @param k int
+     * @param k     int
      * @return int
      */
     private int getKthSmallestNum(int[] nums1, int[] nums2, int k) {
@@ -121,10 +72,10 @@ public class P4_FindMedianInTwoSortedArrays {
                 return Math.min(nums1[offset1], nums2[offset2]);
             }
 
-            /* 将两个数组[offset, index]的数比较，抛弃其中一组小的 */
             int index1 = Math.min(offset1 + (k >> 1) - 1, len1 - 1);
             int index2 = Math.min(offset2 + (k >> 1) - 1, len2 - 1);
 
+            /* 将两个数组[offset, index]的数比较，抛弃其中一组小的 */
             if (nums1[index1] <= nums2[index2]) {
                 k = k - (index1 - offset1 + 1);
                 offset1 = index1 + 1;
@@ -137,8 +88,8 @@ public class P4_FindMedianInTwoSortedArrays {
 
     public static void main(String[] args) {
         P4_FindMedianInTwoSortedArrays t = new P4_FindMedianInTwoSortedArrays();
-        int[] nums1 = new int[]{2, 3, 4, 5, 6};
-        int[] nums2 = new int[]{1};
+        int[] nums1 = new int[]{1, 2};
+        int[] nums2 = new int[]{1, 2, 3};
         System.out.println(t.findMedianSortedArrays(nums1, nums2));
     }
 

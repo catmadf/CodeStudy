0e029984040c586988eda359f9571735f2d03ea0
catmadf <375174102@qq.com>
2020-05-31 23:58:35 +0800

    LeetCode101:对称二叉树-递归解法

---
+++ src/main/java/cn/madf/leetCode/P101_SymmetricTree.java
@@ -0,0 +1,53 @@
+package cn.madf.leetCode;
+
+/**
+ *
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ *
+ *     1
+ *    / \
+ *   2   2
+ *  / \ / \
+ * 3  4 4  3
+ *
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ *
+ *     1
+ *    / \
+ *   2   2
+ *    \   \
+ *    3    3
+ *
+ * @author 烛影鸾书
+ * @date 2020/5/31
+ * @copyright© 2020
+ */
+public class P101_SymmetricTree {
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public boolean isSymmetric(TreeNode root) {
+        TreeNode p = root;
+        TreeNode q = root;
+        return isSymmetricSubTree(p, q);
+    }
+
+    private boolean isSymmetricSubTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null) {
+            return false;
+        }
+        return p.val == q.val && isSymmetricSubTree(p.left, q.right) && isSymmetricSubTree(p.right, q.left);
+    }
+}
